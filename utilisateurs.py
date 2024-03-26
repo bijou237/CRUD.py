1bfeb43a0ade1fed8c8df7036fc262a237cb1946
bijou <bijoueyong.com>
2024-03-26 18:11:22 -0400

    changes

--- utilisateurs.py
+++ utilisateurs.py
@@ -62,26 +62,74 @@ def delete_utilisateur(ID):
     sql = "DELETE FROM users WHERE id=%s"
     cursor.execute(sql, (ID,))
     connexion.commit()
+1
 
+class Utilisateur:
+    def __init__(self, nom, age, email):
+        self.nom = nom
+        self.age = age
+        self.email = email
 
+class GestionUtilisateurs:
+    def __init__(self):
+        self.utilisateurs = []
 
-# main
-email = input("Donnez votre adresse mail :")
-password = input("Donnez votre mot de passe :")
-nom = input("Donnez votre nom complet :")
-utilisateur = create_utilisateur(email,password,nom)  
-#delete_user(4)
-update_utilisateur(1,'test@test.co','1233467','Amadou Diallo')
-read_utilisateur()
-#print(user)
+    def ajouter_utilisateur(self, utilisateur):
+        self.utilisateurs.append(utilisateur)
 
-# ajouter un utilisateur
-def add_user(email, password, nom):
-    
-    cursor = connexion.cursor()
+    def lister_utilisateurs(self):
+        for utilisateur in self.utilisateurs:
+            print(f"Nom: {utilisateur.nom}, Age: {utilisateur.age}, Email: {utilisateur.email}")
 
-    sql = "INSERT INTO users(email, password, nom) VALUES(%s,%s,%s)"
-    cursor.execute(sql, (email,password,nom))
-    connexion.commit()
+    def mettre_a_jour_utilisateur(self, index, nouvel_utilisateur):
+        self.utilisateurs[index] = nouvel_utilisateur
 
-    return (nom, email)
+    def supprimer_utilisateur(self, index):
+        del self.utilisateurs[index]
+
+def afficher_menu():
+    print("Menu :")
+    print("1. Ajouter un utilisateur")
+    print("2. Mettre à jour un utilisateur")
+    print("3. Supprimer un utilisateur")
+    print("4. Lister les utilisateurs")
+    print("5. Quitter")
+
+def main():
+    gestion_utilisateurs = GestionUtilisateurs()
+
+    while True:
+        afficher_menu()
+        choix = input("Choisissez une option : ")
+
+        if choix == "1":
+            nom = input("Nom de l'utilisateur : ")
+            age = input("Âge de l'utilisateur : ")
+            email = input("Email de l'utilisateur : ")
+            utilisateur = Utilisateur(email, password, nom)
+            gestion_utilisateurs.ajouter_utilisateur(utilisateur)
+
+        elif choix == "2":
+            index = int(input("Indiquez l'index de l'utilisateur à mettre à jour : "))
+            nom = input("Nouveau nom de l'utilisateur : ")
+            age = input("Nouvel âge de l'utilisateur : ")
+            email = input("Nouvel email de l'utilisateur : ")
+            nouvel_utilisateur = Utilisateur(email, password, nom)
+            gestion_utilisateurs.mettre_a_jour_utilisateur(index, nouvel_utilisateur)
+
+        elif choix == "3":
+            index = int(input("Indiquez l'index de l'utilisateur à supprimer : "))
+            gestion_utilisateurs.supprimer_utilisateur(index)
+
+        elif choix == "4":
+            gestion_utilisateurs.lister_utilisateurs()
+
+        elif choix == "5":
+            print("Merci d'avoir utilisé le programme.")
+            break
+
+        else:
+            print("Choix invalide. Veuillez choisir une option valide.")
+
+if __name__ == "__main__":
+    main()
